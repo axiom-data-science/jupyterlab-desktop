dc219626abfcc85c8ed604eced4ef348be4b9166
Jesse Lopez <yosoyjay@gmail.com>
2022-03-17 13:10:28 -0700

    More updates for demo to work on windows

--- scripts/post_install.bat
+++ scripts/post_install.bat
@@ -1,14 +1,8 @@
 call %~dp0..\Scripts\activate.bat
-conda activate $PREFIX
 
-# Get extract_model and osma to install
-mkdir src
-git clone -b updates-for-windows https://github.com/axiom-data-science/extract_model.git
-pushd extract_model
-pip install -e .
-popd
+pip install git+https://github.com/axiom-data-science/extract_model.git@updates-for-windows
+pip install git+https://github.com/axiom-data-science/ocean-model-skill-assessor.git@updates-for-windows
+pip uninstall -yq ocean_data_gateway
+pip install git+https://github.com/axiom-data-science/ocean_data_gateway.git@fix-for-app
 
-git clone -b updates-for-windows https://github.com/axiom-data-science/ocean-model-skill-assessor.git
-pushd ocean-model-skill-assessor
-pip install -e .
-popd
+SET CONDA_DELL_SEARCH_MODIFICTION_ENABLE="1"
